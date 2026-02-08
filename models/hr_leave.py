4c3b6514d80da919021f5c09c8d6e4527e458c6f
kirugoit <goitomnathan64@gmail.com>
2026-02-08 07:47:13 -0800

    remove white space

--- models/hr_leave.py
+++ models/hr_leave.py
@@ -8,9 +8,7 @@ class HrLeave(models.Model):
     reason_leave = fields.Text(string="Detailed Reason")
     binary_file = fields.Binary(string="Uplead File")
     binary_file_name = fields.Char(string="Binary File Name")
-    leave_type = fields.Selection([("annual", "Annual"),
-                                   ("sick", "Sick"),
-                                   ("other", "Other")], string="Leave Type")
+    
 
     @api.constrains('number_of_days')
     def _check_leave_balance(self):
